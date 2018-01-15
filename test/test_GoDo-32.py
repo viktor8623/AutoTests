abcb8ac9f093ea72c8a0010086b3570972ab8098
Viktor <viktor8623@mail.ru>
2018-01-15 19:23:59 +0300

    Add new certificate. Created three tests for different payment types.

--- test/test_GoDo-32.py
+++ test/test_GoDo-32.py
@@ -1,30 +1,38 @@
 from model.certificate import Certificate
-import time
 
 
 
-# def test_dead_of_night_tour(app):
-#     """Purchasing certificate with number of tickets (120$, Check)"""
-#     app.certificate.navigate_to()
-#     app.certificate.select_certificate(Certificate(type="Activity Tickets", activity="Dead of Night Tour",
-#                                                    first_tickets_type="3", second_tickets_type="3",
-#                                                    initial_amount="120.00"))
-#     app.certificate.charge_and_save("card") # only cash, check, card
-#     app.certificate.check_if_it_created(Certificate(type="Activity Tickets", activity="Dead of Night Tour",
-#                                                    first_tickets_type="3", second_tickets_type="3",
-#                                                    initial_amount="120.00"))
+def test_purchasing_certificate_cash(app):
+    app.certificate.navigate_to()
+    app.certificate.select_certificate(Certificate(type="Activity Tickets", activity="Dead of Night Tour",
+                                                   first_tickets_type="3", second_tickets_type="3",
+                                                   initial_amount="120.00", name_first_tickets_type="Adult",
+                                                   name_second_tickets_type="Child"))
+    app.certificate.charge_and_save("cash")        # only cash, check, card
+    app.certificate.check_if_it_created(Certificate(type="Activity Tickets", activity="Dead of Night Tour",
+                                                   first_tickets_type="3", second_tickets_type="3",
+                                                   initial_amount="120.00", name_first_tickets_type="Adult",
+                                                   name_second_tickets_type="Child"))
 
-# def test_beyound_good_and_evel_tours(app):
-#     app.certificate.navigate_to()
-#     app.certificate.select_certificate(Certificate(type="Activity Tickets", activity="Dead of Night Tour",
-#                                                    first_tickets_type="3", second_tickets_type="3",
-#                                                    initial_amount="120.00"))
-#     app.certificate.charge_and_save("cash")  # only cash, check, card
-#
-def test_beyound_good_and_evel_tours2(app):
+def test_purchasing_certificate_credit_card(app):
+    app.certificate.navigate_to()
+    app.certificate.select_certificate(Certificate(type="Activity Tickets", activity="Dead of Night Tour",
+                                                   first_tickets_type="3", second_tickets_type="3",
+                                                   initial_amount="120.00", name_first_tickets_type="Adult",
+                                                   name_second_tickets_type="Child"))
+    app.certificate.charge_and_save("card")        # only cash, check, card
+    app.certificate.check_if_it_created(Certificate(type="Activity Tickets", activity="Dead of Night Tour",
+                                                   first_tickets_type="3", second_tickets_type="3",
+                                                   initial_amount="120.00", name_first_tickets_type="Adult",
+                                                   name_second_tickets_type="Child"))
+
+
+def test_purchase_certificate_check(app):
     app.certificate.navigate_to()
     app.certificate.select_certificate(Certificate(type="Activity Tickets", activity="Beyond Good and Evil Tours",
-                                                   second_tickets_type="1"))
-    app.certificate.charge_and_save("check")  # only cash, check, card
+                                                   second_tickets_type="1", initial_amount="20.00",
+                                                   name_second_tickets_type="Child"))
+    app.certificate.charge_and_save("check")       # only cash, check, card
     app.certificate.check_if_it_created(Certificate(type="Activity Tickets", activity="Beyond Good and Evil Tours",
-                                                    second_tickets_type="1", initial_amount="20.00"))
+                                                    second_tickets_type="1", initial_amount="20.00",
+                                                    name_second_tickets_type="Child"))

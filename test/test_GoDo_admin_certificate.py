9ff8599736ee720e7b8d296a9d191ace0a65f6f2
Viktor <viktor8623@mail.ru>
2018-05-13 23:48:40 +0300

    file renamed

--- test/test_GoDo_admin_certificate.py
+++ test/test_GoDo_admin_certificate.py
@@ -1,8 +1,8 @@
 import pytest
-from data.add_certificate import testdata
+from data.certificates import testdata1
 
 
-@pytest.mark.parametrize("certificate", testdata, ids=[repr(x) for x in testdata])
+@pytest.mark.parametrize("certificate", testdata1, ids=[repr(x) for x in testdata1])
 def test_purchasing_certificate(app, certificate):
     """Selling certificates via admin."""
     app.certificate.add_new_certificate(certificate)
